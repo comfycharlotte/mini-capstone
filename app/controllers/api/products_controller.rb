e52e32341e7d31388fa5b9e0ff332282c5d48877
charlotte <charlotte.e.valentine@gmail.com>
2021-02-25 20:49:05 -0600

    add create method to Product class

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -9,4 +9,20 @@ class Api::ProductsController < ApplicationController
     @product = Product.find(product_id)
     render "api/products/show.json.jb"
   end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+
+    if @product.save
+      @message = "Success!"
+    else
+      @message = "An error has been encountered while attempting to save the product."
+    end
+    render "api/products/create.json.jb"
+  end
 end
