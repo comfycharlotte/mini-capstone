ab90705fdae4e10b806080e283d129ea0808cbe9
charlotte <charlotte.e.valentine@gmail.com>
2021-03-08 21:20:26 -0600

    add create method to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#delete"
+
     post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
   end
 end
