0a7ab226047688e74cc629590e142e22f02f23aa
charlotte <charlotte.e.valentine@gmail.com>
2021-03-08 21:08:03 -0600

    add create method to user

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#delete"
+    post "/users" => "users#create"
   end
 end
