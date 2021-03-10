bf3db0a6b5ed5f89a16f534dc611ce65555754b5
charlotte <charlotte.e.valentine@gmail.com>
2021-03-09 21:07:59 -0600

    add orders model

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,2 +1,15 @@
 class Api::OrdersController < ApplicationController
+  def create
+    product = Product.find_by(name: params[:product])
+    order = Order.new(
+      user_id: current_user.id,
+      product_id: product.id
+      quantity: params[:quantity]
+    )
+    if order.create 
+      render json: { message: "Order created successfully"}
+    else
+      render json: { errors: order.errors.full_messages }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
     post "/users" => "users#create"
 
     post "/sessions" => "sessions#create"
+
+    post "/orders" => "orders#create"
   end
 end
